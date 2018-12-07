acdc07ecc14033210253929dd8b04792c8ea70f6
guqueiroz <gu_uqueiroz@hotmail.com>
2018-12-07 11:23:29 -0200

    Add files via upload

---
+++ src/Agenda_full_aws/Agenda/src/java/br/com/agenda/jdbc/FazConexao.java
@@ -0,0 +1,38 @@
+package br.com.agenda.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class FazConexao {
+
+    public static Connection conexao;
+
+    public static Connection getConexao() {
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            conexao = DriverManager.getConnection("jdbc:mariadb://mariadbinstance.crzswjdcnrzp.us-east-2.rds.amazonaws.com:3306/" + "agenda", "root", "12345678");
+            System.out.println("Conectado");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("erro na conexão");
+        }
+
+        return conexao;
+}
+    
+    public static void main(String...a) {
+        FazConexao con = new FazConexao();
+        con.getConexao();
+    }
+}
+    
+
+
+    
+
+    
+    
+
